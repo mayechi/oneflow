5a3681cf89a38f3ccbd3aee6398a9ab4613a30cf
“mayechi” <mayechi@zhejianglab.com>
2020-05-28 10:38:00 +0800

    update

--- label_server.py
+++ label_server.py
@@ -33,7 +33,7 @@ des_folder = os.path.join('./log', args.mode)
 if not os.path.exists(des_folder):
     os.makedirs(des_folder)
 logging.basicConfig(level=logging.DEBUG,#控制台打印的日志级别
-                    filename=des_folder+'./labellog.txt',
+                    filename=des_folder+'/labellog.txt',
                     filemode='w',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
                     #a是追加模式，默认如果不写的话，就是追加模式
                     format=
